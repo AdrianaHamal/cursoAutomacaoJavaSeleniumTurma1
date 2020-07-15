f5ed2d2765288cec5ec23f983c6d9a1aee9aeabd
Adriana Hamal <ahamal85@gmail.com>
2020-07-15 17:55:16 -0300

    quinto commite

--- src/test/java/core/Element.java
+++ src/test/java/core/Element.java
@@ -1,46 +1,92 @@
 package core;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import enums.ByValue;
 
 public class Element {
 	ByValue by;
 	String map;
+	boolean lista = false;
+	WebElement webElement = null;
+	
+	
+	public void setWebElement(WebElement _element) {
+		webElement = _element;
+	}
 
 	public Element(ByValue by, String _map) {
 		this.by = by;
 		map = _map;
-
 	}
+	
 
+	
 	public WebElement getElement() {
 		WebElement element = null;
 		switch (by) {
 		case ID:
-			element = Driver.getDriver().findElement(By.id(map));
+			element = get(By.id(map));
 			break;
 		case XPATH:
-			element = Driver.getDriver().findElement(By.xpath(map));
+			element = get(By.xpath(map));
 			break;
 		case CSS:
-			element = Driver.getDriver().findElement(By.cssSelector(map));
+			element = get(By.cssSelector(map));
 			break;
 		case LINKTEXT:
-			element = Driver.getDriver().findElement(By.linkText(map));
+			element = get(By.linkText(map));
 			break;
 		case NAME:
-			element = Driver.getDriver().findElement(By.name(map));
+			element = get(By.name(map));
 			break;
 		default:
 			break;
 		}
 
 		return element;
-
 	}
 
+	public WebElement get(By by) {
+		if (webElement == null) {
+			return Driver.getDriver().findElement(by);
+		}else {
+			return webElement.findElement(by);
+		}
+	}
+	
+	
+	public List<WebElement> getElements() {
+		List<WebElement> elements = null;
+		switch (by) {
+		case ID:
+			elements = Driver.getDriver().findElements(By.id(map));
+			break;
+		case XPATH:
+			elements = Driver.getDriver().findElements(By.xpath(map));
+			break;
+		case CSS:
+			elements = Driver.getDriver().findElements(By.cssSelector(map));
+			break;
+		case LINKTEXT:
+			elements = Driver.getDriver().findElements(By.linkText(map));
+			break;
+		case NAME:
+			elements = Driver.getDriver().findElements(By.name(map));
+			break;
+		default:
+			break;
+		}
+
+		return elements;
+		
+		
+
+	}
 	public void sendKeys(CharSequence... value) {
 		getElement().sendKeys(value);
 	}
@@ -53,4 +99,32 @@ public class Element {
 		getElement().click();
 	}
 
+	public void getAttribute(String value) {
+		getElement().getAttribute(value);
+	}
+	
+	public void clear() {
+		getElement().clear();
+	
+	}
+	
+	public boolean isEnabled() {
+		return getElement().isEnabled();
+	}
+	
+	public boolean isDisplayed() {
+		return getElement().isDisplayed();
+		
+	}
+	
+	public boolean isSelected() {
+		return getElement().isSelected();
+	}
+	
+	public void select(String value) {
+		Select select = new Select(getElement());
+		select.selectByVisibleText(value);
+	}
+	
 }
+
